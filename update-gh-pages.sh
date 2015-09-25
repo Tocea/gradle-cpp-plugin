06be7b0122eb3bdbef33b1f3faa61908cbaaa50b
jguidoux <jeremie.guidoux@tocea.com>
2015-09-25 17:54:36 +0200

    add script to push spock-reports with travis

---
+++ update-gh-pages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R build/spock-reports $HOME/spock-reports
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Tocea/gradle-cpp-plugin.git  gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/spock-reports/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with coverage\n"
+fi
