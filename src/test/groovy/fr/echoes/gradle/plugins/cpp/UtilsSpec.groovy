fb78e4535dcada9fdfcaa1df9e5ee80047d447fd
jguidoux <jeremie.guidoux@tocea.com>
2015-09-25 15:18:09 +0200

    add bdd comment UtilsSpec

--- src/test/groovy/fr/echoes/gradle/plugins/cpp/UtilsSpec.groovy
+++ src/test/groovy/fr/echoes/gradle/plugins/cpp/UtilsSpec.groovy
@@ -13,31 +13,29 @@ class UtilsSpec extends Specification{
     }
 
     def "add age property to a class"() {
-        given:
+
+        given: "a person call 'halyna'"
         def person = new Person(name: "halyna")
 
-        when:
+        when: "I had dinamically the age attribute to 'halyna' ste at '2'"
         person.metaClass.age = 2
 
-        then:
+        then: "'halyna age must be '2'"
         person.age == 2
     }
 
     def "find properties number when added dinamcally"() {
-        given:
 
+        given: "a person call 'halyna'"
         def person = new Person(name: "halyna")
 
-        when:
+        when: "I had dinamically the age attribute to 'halyna' ste at '2'"
         person.metaClass.age = 2
-        person.properties.each {
-            println it.key
-        }
 
         println "mon nom est ${person.name}"
 
 
-        then:
+        then: "'halyna properties must contain the 'age' attribute"
         person.properties.containsKey("age")
     }
 }
