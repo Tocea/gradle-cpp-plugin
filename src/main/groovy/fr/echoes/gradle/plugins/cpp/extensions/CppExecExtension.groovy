36d0a245704ca1ccb84817cedc370900cd3f68ba
jguidoux <jeremie.guidoux@tocea.com>
2015-10-01 12:40:43 +0200

    * move dynamicals propperties of CppExecDeclaration into his constructor

--- src/main/groovy/fr/echoes/gradle/plugins/cpp/extensions/CppExecExtension.groovy
+++ src/main/groovy/fr/echoes/gradle/plugins/cpp/extensions/CppExecExtension.groovy
@@ -1,5 +1,9 @@
 package fr.echoes.gradle.plugins.cpp.extensions
 
+import fr.echoes.gradle.plugins.cpp.tasks.CppExecTask
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskCollection
+
 /**
  * Created by jguidoux on 01/10/15.
  */
@@ -7,5 +11,18 @@ class CppExecExtension {
     def execPath = ""
     Map<String, ?> env
 
+    CppExecExtension(Project _project) {
+
+
+
+        TaskCollection tasks = _project.tasks.withType(CppExecTask)
+        tasks.each {
+            this.metaClass."${it.name}ExecPath" = ""
+            this.metaClass."${it.name}BaseArgs" = null
+            this.metaClass."${it.name}Args" = ""
+            this.metaClass."${it.name}StandardOutput" = null
+            this.metaClass."${it.name}ExecWorkingDir" = null
+        }
+    }
 }
 
--- src/main/groovy/fr/echoes/gradle/plugins/cpp/extensions/CppPluginExtension.groovy
+++ src/main/groovy/fr/echoes/gradle/plugins/cpp/extensions/CppPluginExtension.groovy
@@ -1,12 +1,19 @@
 package fr.echoes.gradle.plugins.cpp.extensions
 
-import fr.echoes.gradle.plugins.cpp.CppPluginUtils
 import fr.echoes.gradle.plugins.cpp.model.ApplicationType
 import fr.echoes.gradle.plugins.cpp.tasks.CppExecTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskCollection
 
 /**
+ * extension to configure the gradle-cpp-plugin.
+ *
+ * This extension can be used like this in build.gradle file.
+ * <pre>
+ *     cpp {
+ *
+ *     }
+ * </pre>
  * Created by jguidoux on 01/10/15.
  */
 class CppPluginExtension {
@@ -17,18 +24,9 @@ class CppPluginExtension {
     def outPutDirs = new HashMap()
     CppExecExtension exec
 
-    private  Project project
-    CppPluginExtension(Project _project) {
-        project = _project
-        exec = new CppExecExtension()
-        TaskCollection tasks = _project.tasks.withType(CppExecTask)
-        tasks.each {
-            exec.metaClass."${it.name}ExecPath" = ""
-            exec.metaClass."${it.name}BaseArgs" = null
-            exec.metaClass."${it.name}Args" = ""
-            exec.metaClass."${it.name}StandardOutput" = null
-            exec.metaClass."${it.name}ExecWorkingDir" = null
-        }
+    CppPluginExtension( Project _project) {
+        exec = new CppExecExtension(_project)
+
     }
 
 
