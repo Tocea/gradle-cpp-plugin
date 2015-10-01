816b34d7ae670ab1bea9b99e437f7a38c0f27ecc
jguidoux <jeremie.guidoux@tocea.com>
2015-10-01 15:08:08 +0200

    + clean CppPlugin class

--- src/main/groovy/fr/echoes/gradle/plugins/cpp/CppPlugin.groovy
+++ src/main/groovy/fr/echoes/gradle/plugins/cpp/CppPlugin.groovy
@@ -3,8 +3,6 @@ package fr.echoes.gradle.plugins.cpp
 import fr.echoes.gradle.plugins.cpp.configurations.ArchivesConfigurations
 import fr.echoes.gradle.plugins.cpp.configurations.CppConfiguration
 import fr.echoes.gradle.plugins.cpp.extensions.CppPluginExtension
-import fr.echoes.gradle.plugins.cpp.model.ApplicationType
-import com.tocea.gradle.plugins.cpp.tasks.*
 import fr.echoes.gradle.plugins.cpp.tasks.CppExecTask
 import fr.echoes.gradle.plugins.cpp.tasks.DownloadLibTask
 import fr.echoes.gradle.plugins.cpp.tasks.InitOutputDirsTask
@@ -15,7 +13,6 @@ import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.internal.plugins.DslObject
 import org.gradle.api.plugins.MavenRepositoryHandlerConvention
 import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.TaskCollection
 import org.gradle.api.tasks.Upload
 import org.gradle.api.tasks.bundling.Zip
 
@@ -61,9 +58,8 @@ class CppPlugin implements Plugin<Project> {
             } else {
                 println "no build tasks"
             }
-
             List dependentProjects = findDependentProjects(_project)
-            _project.tasks["downloadLibs"].dependsOn dependentProjects.tasks["build"]
+            _project.tasks.downloadLibs.dependsOn dependentProjects.tasks.build
 
         }
 
@@ -73,18 +69,18 @@ class CppPlugin implements Plugin<Project> {
 
         if (_project.cpp.outPutDirs) {
             CppPluginUtils.OUTPUT_DIRS << _project.cpp.outPutDirs
-            _project.tasks["initOutputDirs"].outpoutDirs = CppPluginUtils.OUTPUT_DIRS
+            _project.tasks.initOutputDirs.outpoutDirs = CppPluginUtils.OUTPUT_DIRS
             if (CppPluginUtils.OUTPUT_DIRS[CppPluginUtils.EXT_LIB_DIR]) {
-                DownloadLibTask dlTask = _project.tasks["downloadLibs"]
+                DownloadLibTask dlTask = _project.tasks.downloadLibs
                 dlTask.changeExtLibLocation(CppPluginUtils.OUTPUT_DIRS[CppPluginUtils.EXT_LIB_DIR])
             }
         }
 
-       Copy copyHeaders =  _project.tasks["copyHeaders"]
+       Copy copyHeaders =  _project.tasks.copyHeaders
         copyHeaders.from(CppPluginUtils.SOURCE_HEADERS)
         copyHeaders.into("${_project.buildDir}" +
                 "/${CppPluginUtils.OUTPUT_DIRS[CppPluginUtils.TMP_DIR]}" +
-                "/${CppPluginUtils.OUTPUT_TMP_DIRS["headers"]}")
+                "/${CppPluginUtils.OUTPUT_TMP_DIRS.headers}")
     }
 
     private void projectConfigurations(Project _project) {
@@ -103,38 +99,38 @@ class CppPlugin implements Plugin<Project> {
         _project.task('testCpp', type: CppExecTask, group: 'build')
         _project.task('cppArchive', type: Zip, group: 'archives')
         configureInstall(_project)
-        configureBuildTasks(_project)
         configureTasksDependencies(_project)
 
     }
 
-    def configureBuildTasks(Project _project) {
-
-
-    }
 
     private configureTasksDependencies(Project _project) {
 
 
-        _project.tasks["downloadLibs"].dependsOn _project.tasks["initOutputDirs"]
+        _project.tasks.downloadLibs.dependsOn _project.tasks.initOutputDirs
 
 
-        _project.tasks["cppArchive"].dependsOn _project.tasks["downloadLibs"]
-        _project.tasks["assemble"].dependsOn _project.tasks["cppArchive"]
-        _project.tasks["install"].dependsOn _project.tasks["assemble"]
-//        _project.tasks["build"].dependsOn _project.tasks["install"]
-        _project.tasks["uploadArchives"].dependsOn _project.tasks["build"]
+        _project.tasks.cppArchive.dependsOn _project.tasks.downloadLibs
+        _project.tasks.assemble.dependsOn _project.tasks.cppArchive
+        _project.tasks.install.dependsOn _project.tasks.assemble
+        _project.tasks.uploadArchives.dependsOn _project.tasks.build
 
     }
 
     def configureBuildTasksDependencies(final Project _project) {
-        _project.tasks["compileCpp"].dependsOn _project.tasks["validateCMake"]
-        _project.tasks["compileCpp"].dependsOn _project.tasks["downloadLibs"]
-        _project.tasks["testCompileCpp"].dependsOn _project.tasks["compileCpp"]
-        _project.tasks["testCpp"].dependsOn _project.tasks["testCompileCpp"]
-        _project.tasks["check"].dependsOn _project.tasks["testCpp"]
-        _project.tasks["cppArchive"].dependsOn _project.tasks["copyHeaders"]
-        _project.tasks["cppArchive"].dependsOn _project.tasks["compileCpp"]
+        _project.tasks.compileCpp.dependsOn _project.tasks.validateCMake
+        _project.tasks.compileCpp.dependsOn _project.tasks.downloadLibs
+        _project.tasks.testCompileCpp.dependsOn _project.tasks.compileCpp
+        _project.tasks.testCpp.dependsOn _project.tasks.testCompileCpp
+        _project.tasks.check.dependsOn _project.tasks.testCpp
+        _project.tasks.cppArchive.dependsOn _project.tasks.copyHeaders
+        _project.tasks.cppArchive.dependsOn _project.tasks.compileCpp
+    }
+
+    private List findDependentProjects(Project _project) {
+        def projectDependencies = _project.configurations.compile.allDependencies.withType(ProjectDependency)
+        def dependentProjects = projectDependencies*.dependencyProject
+        dependentProjects
     }
 
     private void configureInstall(Project project) {
@@ -145,11 +141,7 @@ class CppPlugin implements Plugin<Project> {
         installUpload.description = "Installs the 'archives' artifacts into the local Maven repository."
     }
 
-    private List findDependentProjects(Project _project) {
-        def projectDependencies = _project.configurations.compile.allDependencies.withType(ProjectDependency)
-        def dependentProjects = projectDependencies*.dependencyProject
-        dependentProjects
-    }
+
 
 
 }
