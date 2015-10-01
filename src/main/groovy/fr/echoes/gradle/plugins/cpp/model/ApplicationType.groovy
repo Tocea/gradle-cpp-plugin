a0d6cf9dbfda554c9c3f575daf374565fa3136e2
jguidoux <jeremie.guidoux@tocea.com>
2015-10-01 18:12:49 +0200

    + javadoc for ApplicatonType class

--- src/main/groovy/fr/echoes/gradle/plugins/cpp/model/ApplicationType.groovy
+++ src/main/groovy/fr/echoes/gradle/plugins/cpp/model/ApplicationType.groovy
@@ -1,6 +1,8 @@
 package fr.echoes.gradle.plugins.cpp.model
 
 /**
+ * Enum to store the two kinds of c application 'clibrary' or 'capplication'
+ * <p>
  * Created by jguidoux on 06/09/15.
  */
 enum ApplicationType {
